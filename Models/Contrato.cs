4f9c85e473d95a78519a7d0cb5f44d56f68c9e11
CarlosBenenatti-hub <carlosbenenatti21@gmail.com>
2025-08-31 17:06:03 -0300

    Creacion de Models
    Cree los models de inmueble y contrato

---
+++ Models/Contrato.cs
@@ -0,0 +1,13 @@
+namespace ProyectoInmobiliaria.Models
+{
+    public class Contrato
+    {
+        public int IdContrato { get; set; }         
+        public int IdInquilino { get; set; }          
+        public int IdInmueble { get; set; }           
+
+        public decimal Monto { get; set; }            
+        public DateTime FechaDesde { get; set; }      
+        public DateTime FechaHasta { get; set; }      
+    }
+}
---
+++ Models/Inmueble.cs
@@ -0,0 +1,14 @@
+namespace ProyectoInmobiliaria.Models
+{
+    public class Inmueble
+    {
+        public int IdInmueble { get; set; }
+        public int IdEstadoInmueble { get; set; }
+        public int IdTipoInmueble { get; set; }
+        public int IdPropietario { get; set; }
+
+        public string Direccion { get; set; } = "";
+        public int CantidadAmbientes { get; set; }
+        public decimal SuperficieM2 { get; set; }
+    }
+}
